a04eb19a44dcd161579c279722cf37955ed4f116
Arkadiusz Cios <arkadiuszcios@outlook.com>
2025-12-24 14:36:28 +0100

    feat: #dev add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import SessionProviderWrapper from "@/components/SessionProviderWrapper";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gray-50 dark:bg-gray-900`}
       >
         <SessionProviderWrapper>
+          <Navbar />
           {children}
         </SessionProviderWrapper>
       </body>
