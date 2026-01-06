bd38a4edef9e7fe95d53fe7e0d06dec35c4878ea
Arkadiusz Cios <arkadiuszcios@outlook.com>
2026-01-06 18:03:11 +0100

    refactor: #dev move zod scheams to validation dir

--- src/repositories/review.repository.ts
+++ src/repositories/review.repository.ts
@@ -1,6 +1,6 @@
 import "server-only";
 import prisma from "@/lib/prisma";
-import { CreateReviewSchema, CreateReviewInput } from "./review.schema";
+import { CreateReviewSchema, CreateReviewInput } from "../validation/review.schema";
 
 export async function getAllReviews() {
   return await prisma.review.findMany({});
